e2dbf6690613265fa17b252bc123a2c0c2bcef40
Div <sh.kim.x928@gmail.com>
2023-10-15 17:58:21 +0900

    Revert "feat: add remix/grunge-stack"
    This reverts commit 5fb438c70f31a02c747c30b5d62fd2a9b3f32da8.

--- remix.config.ts
+++ remix.config.ts
@@ -1,24 +1,21 @@
-import path from "node:path";
-
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
-  publicPath: "/_static/build/",
-  server: "server.ts",
-  serverBuildPath: "build/index.mjs",
-  serverModuleFormat: "esm",
-  routes: (defineRoutes) =>
-    defineRoutes((route) => {
-      if (process.env.NODE_ENV === "production") return;
-
-      console.log("⚠️  Test routes enabled.");
-
-      const appDir = path.join(process.cwd(), "app");
-
-      route(
-        "__tests/create-user",
-        path.relative(appDir, "cypress/support/test-routes/create-user.ts")
-      );
-    }),
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+module.exports = {
+  appDirectory: "app",
+  browserBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002,
+  serverDependenciesToBundle: [
+    "remix-utils",
+    "is-ip",
+    "ip-regex",
+    "super-regex",
+    "clone-regexp",
+    "function-timeout",
+    "time-span",
+    "convert-hrtime",
+    "is-regexp",
+  ],
 };
